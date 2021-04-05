d4338a4afea091bde2d13bd6fc03fec5dc3a9856
Joseph Haller <j.edward.haller@gmail.com>
2021-04-05 16:13:00 -0400

    Built out basic add group form

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,10 +3,7 @@ import {
   Navbar,
   NavbarBrand,
   Nav,
-  NavbarToggler,
-  Collapse,
   NavItem,
-  Jumbotron,
   Button,
   Modal,
   ModalHeader,
@@ -17,7 +14,12 @@ import {
   Label,
   NavLink
 } from 'reactstrap';
-// import { NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class Header extends Component {
   constructor(props) {
@@ -25,10 +27,17 @@ class Header extends Component {
 
     this.state = {
       isModalOpen: false,
+      input: {
+        group_name: '',
+        description: '',
+        category: ''
+      },
+      options: ['outdoors', 'music', 'cooking', 'animals', 'hobbies', 'religious']
     };
 
     this.toggleModal = this.toggleModal.bind(this);
     this.handleCreateGroup = this.handleCreateGroup.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
 
@@ -40,99 +49,109 @@ class Header extends Component {
 
   handleCreateGroup(e) {
     this.toggleModal();
+    console.log(this.state.input);
     e.preventDefault();
+  }
+
+  handleChange(e) {
+    let newInput = this.state.input;
+    newInput[e.target.name] = e.target.value;
+    this.setState({
+      input: newInput
+    });
   }
 
   render() {
     return (
-      <div className="main-header">
-        <Navbar  className="py-3" color="primary" expand='md'>
-          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-            <ModalHeader toggle={this.toggleModal}>Create New Group</ModalHeader>
-            <ModalBody>
-              <Form onSubmit={this.handleCreateGroup}>
-                <FormGroup>
-                  <Label for='group-name'>Group Name</Label>
-                  <Input
-                    type='text'
-                    id='group-name'
-                    name='group-name'
-                    innerRef={(input) => (this.username = input)}
-                  />
+      <div className='main-header'>
+        <Router>
+          <Navbar  className='py-3' color='primary' expand='md'>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+              <ModalHeader toggle={this.toggleModal}>Create New Group</ModalHeader>
+              <ModalBody>
+                <Form onSubmit={this.handleCreateGroup}>
+                  <FormGroup>
+                    <Label for='group-name'>Group Name</Label>
+                    <Input
+                      type='text'
+                      id='group-name'
+                      name='name'
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for='group-description'>Group Description</Label>
+                    <Input
+                      type='textarea'
+                      id='group-description'
+                      name='description'
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                  <Label for='modal-category'>Select Category</Label>
+                    <Input onChange={this.handleChange} type='select' name='category' id='modal-category'>
+                      {this.state.options.map((option, i) => {
+                        return <option key={i}>{option}</option>
+                      })}
+                    </Input>
                 </FormGroup>
-                <FormGroup>
-                  <Label for='password'>Group Description</Label>
-                  <Input
-                    type='textarea'
-                    id='group-description'
-                    name='group-description'
-                    innerRef={(input) => (this.password = input)}
-                  />
-                </FormGroup>
-                <FormGroup>
-                <Label for="modal-category">Select Category</Label>
-                  <Input type="select" name="select" id="modal-category">
-                    <option>outdoors</option>
-                    <option>music</option>
-                    <option>cooking</option>
-                    <option>animal</option>
-                    <option>hobbies</option>
-                    <option>religious</option>
-                  </Input>
-              </FormGroup>
-                <Button type='submit' value='submit' color='primary'>
-                  Create Group
-                </Button>
-              </Form>
-            </ModalBody>
-          </Modal>
-            <Nav navbar>
-              <NavItem>
-              <NavbarBrand
-              className='mx-auto'
-              style={{ color: '#fff', transform: "translateX(-50%)",
-              left: "50%",
-              position: "absolute",
-              bottom: "10%",
-              fontSize: "2em" }}
-              href="/">
-              &#91;&#160;&#160;&#93; TownSquare
-              </NavbarBrand>
-              </NavItem>
-            </Nav>
-          <div className="ml-auto">
-            <Nav navbar>
-              <NavItem>
-                <Button
-                  outline
-                  color="secondary"
-                  size="small"
-                  style={{ backgroundColor: '#fff'}}
-                  // onClick={this.toggleModal}
-                  href="/allgroups">
-                  Browse All Groups
-                </Button>
-              </NavItem>
-              <NavItem>
-                <Button
-                  outline
-                  color="secondary"
-                  size="small"
-                  style={{ backgroundColor: '#fff', marginLeft: "10px"}}
-                  onClick={this.toggleModal}
-                  className="createGroupBtn"
-                >
-                  Create New Group
-                </Button>
-              </NavItem>
-              <NavItem >
-              <NavLink href="/">
-                  <i style={{color: "#fff"}} className="fas fa-home fa-3x"></i>
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </div>
-        </Navbar>
+                  <Button type='submit' value='submit' color='primary'>
+                    Create Group
+                  </Button>
+                </Form>
+              </ModalBody>
+            </Modal>
+              <Nav navbar>
+                <NavItem>
+                  <NavbarBrand
+                  className='mx-auto'
+                  style={{ color: '#fff', transform: 'translateX(-50%)',
+                  left: '50%',
+                  position: 'absolute',
+                  bottom: '15%',
+                  fontSize: '2em' }}
+                  href='/'
+                  >
+                  &#91;&#160;&#160;&#93; TownSquare
+                  </NavbarBrand>
+                </NavItem>
+              </Nav>
+            <div className='ml-auto'>
+              <Nav navbar>
+                <NavItem>
+                  <Link to='/allgroups'>
+                    <Button
+                      outline
+                      color='secondary'
+                      size='small'
+                      style={{ backgroundColor: '#fff', marginTop: '16px'}}
+                      >
+                      Browse All Groups
+                    </Button>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Button
+                    outline
+                    color='secondary'
+                    size='small'
+                    style={{ backgroundColor: '#fff', marginLeft: '10px', marginTop: '16px'}}
+                    onClick={this.toggleModal}
+                    className='createGroupBtn'
+                  >
+                    Create New Group
+                  </Button>
+                </NavItem>
+                <NavItem >
+                <Link to='/'>
+                    <i style={{color: '#fff', marginLeft: '10px', marginTop: '9px'}} className='fas fa-home fa-3x'></i>
+                  </Link>
+                </NavItem>
+              </Nav>
+            </div>
+          </Navbar>
+        </Router>
       </div>
     );
   }
