d133cc1323dafaf8ffb6eb577696a7c8f20ff120
Stephen Hyde <shh2112@gmail.com>
2021-04-05 08:12:41 -0400

    reformat group data response

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -26,13 +26,29 @@ const getAllGroups = (req, res) => {
 
 const getGroupById = (req, res) => {
   const user_id = req.params.id;
-  connection.query('SELECT * FROM groups_table WHERE user_id = ?', [user_id], (err, results) => {
+  connection.query('SELECT g.group_name, g.description, g.category, g.owner_id, u.first_name, u.last_name, u.email FROM groups_table g LEFT JOIN users u ON g.owner_id = u.user_id WHERE g.owner_id = ?', [user_id], (err, results) => {
     if (err) {
       return res.json({
         error: err
       });
     }
-    return res.json(results);
+    if (results.length === 1) {
+      const row = results[0];
+      return res.json({
+        group_name: row.group_name,
+        description: row.description,
+        category: row.category,
+        owner: {
+          user_id: row.owner_id,
+          first_name: row.first_name,
+          last_name: row.last_name,
+          email: row.email
+        }
+      });
+    }
+    return res.json({
+      error: `no group found with id ${user_id}`
+    });
   });
 };
 
