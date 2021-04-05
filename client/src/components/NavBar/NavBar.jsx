f38096c350151a78bb2e9a7cf1d9a7614b792bd0
Joseph Haller <j.edward.haller@gmail.com>
2021-04-05 11:30:29 -0400

    Added NavBar folder

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="nav-bar">
+
+      </div>
+    )
+  }
+}
+
+export default NavBar;
