a5b7b0a2db805c4a67b18b14e45e464682fa3601
Joseph Haller <j.edward.haller@gmail.com>
2021-04-05 17:34:38 -0400

    Seeded local DB and installed axios

--- client/src/components/ExploreGroups/ExploreGroups.jsx
+++ client/src/components/ExploreGroups/ExploreGroups.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { Jumbotron, Button, ListGroup, ListGroupItem } from 'reactstrap';
 import Header from '../Header/Header.jsx'
 
+const jumbostyles = {
+  width: '80%',
+  display: 'flex',
+  justifyContent: 'center',
+  justifySelf: "center"
+}
+
+const divstyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignContent: 'center',
+  height: '80px'
+}
+
 class ExploreGroups extends React.Component {
   constructor(props) {
     super(props);
@@ -18,12 +32,14 @@ class ExploreGroups extends React.Component {
 
     return (
       <div>
-        <div className="groups-area">
-          <br></br><br></br>
-          <Jumbotron>
-            <h2 className="display-6">Groups In Your Area</h2>
-
+        <br></br><br></br>
+        <div style={divstyles} className='groups-area'>
+          <Jumbotron style={jumbostyles} fluid>
+            <h2 className='display-6'>Groups In Your Area</h2>
           </Jumbotron>
+          <ListGroup>
+
+          </ListGroup>
 
         </div>
       </div>
