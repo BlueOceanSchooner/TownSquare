ee0151aec9310cdcfe78239fc10e342bd03ed571
Stephen Hyde <shh2112@gmail.com>
2021-04-05 15:06:35 -0400

    switch connections to pooled

--- db/connection.js
+++ db/connection.js
@@ -13,7 +13,8 @@ if (!process.env.hasOwnProperty('DB_PASS')) {
   throw Error('.env fiel missing DB_PASS');
 }
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+  connectionLimit: 10,
   host: 'localhost',
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
