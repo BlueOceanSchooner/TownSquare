4ebbad12b6538d29149a65cdea3f92218c7ebaeb
Joseph Haller <j.edward.haller@gmail.com>
2021-04-05 19:54:58 -0400

    Did stuff in Explore groups

--- client/src/components/ExploreGroups/ExploreGroups.jsx
+++ client/src/components/ExploreGroups/ExploreGroups.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Jumbotron, Button, ListGroup, ListGroupItem } from 'reactstrap';
+import { Jumbotron, Button, ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading } from 'reactstrap';
 import Header from '../Header/Header.jsx'
+import axios from 'axios';
 
 const jumbostyles = {
   width: '80%',
@@ -20,8 +21,18 @@ class ExploreGroups extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      groups: null
     }
+  }
+
+  componentDidMount() {
+    axios.get('/api/groups')
+      .then((results) => {
+        console.log(results);
+        this.setState({
+          groups: results.data
+        })
+      })
   }
 
 
@@ -29,21 +40,36 @@ class ExploreGroups extends React.Component {
 
 
   render() {
-
-    return (
-      <div>
-        <br></br><br></br>
-        <div style={divstyles} className='groups-area'>
-          <Jumbotron style={jumbostyles} fluid>
-            <h2 className='display-6'>Groups In Your Area</h2>
-          </Jumbotron>
-          <ListGroup>
-
-          </ListGroup>
-
+    if (!this.state.groups) {
+      return <div>loading...</div>
+    } else {
+      return (
+        <div>
+          <br></br><br></br>
+          <div style={divstyles} className='groups-area'>
+            <Jumbotron style={jumbostyles} fluid>
+              <h2 className='display-6'>Groups In Your Area</h2>
+            </Jumbotron>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div>
+            <ListGroup flush>
+              {this.state.groups.map((group, i) => {
+                return (
+                  <ListGroupItem key={i}>
+                  <ListGroupItemHeading >{group.group_name}</ListGroupItemHeading>
+                  <ListGroupItemText>{group.description}</ListGroupItemText>
+                  </ListGroupItem>
+                )
+              })}
+            </ListGroup>
+          </div>
         </div>
-      </div>
-    )
+      )
+
+    }
 
   }
 };
