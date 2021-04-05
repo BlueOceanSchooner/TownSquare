52212eb399a4358fede0927ff82f766ab259216d
Stephen Hyde <shh2112@gmail.com>
2021-04-05 00:36:58 -0400

    create mysql connection

---
+++ db/connection.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+if (!process.env.hasOwnProperty('DB_NAME')) {
+  throw Error('.env file missing DB_NAME');
+}
+
+if (!process.env.hasOwnProperty('DB_USER')) {
+  throw Error('.env file missing DB_USER');
+}
+
+if (!process.env.hasOwnProperty('DB_PASS')) {
+  throw Error('.env fiel missing DB_PASS');
+}
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS
+});
+
+export default connection;
