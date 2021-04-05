6147d649cb5c6b3928d30f9546d51150b930af05
Stephen Hyde <shh2112@gmail.com>
2021-04-05 09:28:10 -0400

    add routes for events and members

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const users = require('./users.js');
 const groups = require('./groups.js');
+const events = require('./events.js');
+const members = require('./members.js');
 
 router.get('/users', users.getAllUsers);
 router.get('/users/:id', users.getUserById);
@@ -10,5 +12,14 @@ router.post('/users', users.addUser);
 router.get('/groups', groups.getAllGroups);
 router.get('/groups/:id', groups.getGroupById);
 router.post('/groups', groups.addGroup);
+
+router.get('/events', events.getAllEvents);
+router.get('/events/:id', events.getEventById);
+router.get('/groups/:id/events', events.getEventsByGroup);
+router.post('/events', events.addEvent);
+
+router.get('/groups/:id/members', members.getGroupMembers);
+router.get('/users/:id/groups', members.getUserGroups);
+router.post('/users/:user_id/groups/:group_id', members.addUserToGroup);
 
 module.exports = router;
