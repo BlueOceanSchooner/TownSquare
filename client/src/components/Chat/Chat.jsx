b8c241e42cc2bfa540a5acce8ea6729a0da615c6
Colleen Handel <handelc22@gmail.com>
2021-04-05 22:40:40 -0500

    Refactored chat modal by pulling message previews into separate component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { Button, Modal, ModalHeader, ModalBody, Input } from 'reactstrap';
 
+import Previews from './Previews.jsx';
+
 
 class Chat extends React.Component {
   constructor(props) {
@@ -22,8 +24,10 @@ class Chat extends React.Component {
   getMessages() {
     axios.get(`/api/users/${this.props.userID}/dms`)
     .then(response => {
-      var active = response.data ? Object.keys(response.data)[0] : 0;
-      this.setState({ chats: response.data, active })
+      if (this.state.active === 0 && response.data) {
+        this.setState({ chats: response.data, active: Object.keys(response.data)[0] })
+      }
+      this.setState({ chats: response.data })
     })
     .catch(err => console.log('error:', err));
   }
@@ -75,27 +79,8 @@ class Chat extends React.Component {
             Messages
           </ModalHeader>
           <ModalBody className={"modal-body"}>
-            <div className="messages">
-              {Object.keys(this.state.chats).map(otherUserID => {
-                let conversation = this.state.chats[otherUserID];
-                return (
-                  <div className="message" key={otherUserID} name={otherUserID} onClick={this.changeActiveConversation}>
-                    <span className={otherUserID === this.state.active ? "active-bar" : null}></span>
-                    <i className="user-picture fas fa-user"></i>
-                    <div className="content">
-                      <span className="other-user-name">
-                        {conversation[0].sender.user_id === this.props.userID ? `${conversation[0].receiver.first_name} ${conversation[0].receiver.last_name}` : `${conversation[0].sender.first_name} ${conversation[0].sender.last_name}`}
-                      </span>
-                      <br/>
-                      <span className="last-message">
-                        {conversation[conversation.length - 1].sender.user_id === this.props.userID ? "You: " : `${conversation[conversation.length - 1].sender.first_name} ${conversation[conversation.length - 1].sender.last_name}: `}
-                        {conversation[conversation.length - 1].message}
-                      </span>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
+
+           <Previews userID={this.props.userID} chats={this.state.chats} active={this.state.active} changeActiveConversation={this.changeActiveConversation}/>
 
             <div className="conversation">
               <div className="conversation-header">{activeName}</div>
