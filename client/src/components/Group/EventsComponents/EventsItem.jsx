c2a56c981c74703d9e4e9292443f7a9cbb0f27da
Adrian Rodriguez <justpickmeanemailaddress@gmail.com>
2021-04-06 23:04:02 -0400

    Installed moment.js and formatted event times

--- client/src/components/Group/EventsComponents/EventsItem.jsx
+++ client/src/components/Group/EventsComponents/EventsItem.jsx
@@ -4,52 +4,21 @@ import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, CardHeader, Button, ListGroup, ListGroupItem, Container, Row, Col
 } from 'reactstrap';
-
-const formatDate = function (time) {
-  return new Date(time).toLocaleDateString('en-gb',
-    {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-}
-
-const formatTime = function (time) {
-  console.log('event.time:', time);
-
-  const dateTime = new Date(parseInt(time));
-  const hours = dateTime.getUTCHours();
-  const minutes = dateTime.getUTCMinutes();
-
-  console.log('hours', dateTime.getUTCHours());
-  console.log('minutes', dateTime.getUTCMinutes());
-  // if (hours > 12) {
-  //   hours = hours - 12;
-  //   console.log('new hours:', hours);
-  // };
-
-  // var date = new Date(parseInt(time));
-  // return date.toLocaleTimeString(navigator.language, {
-  //   hour: '2-digit',
-  //   minute:'2-digit'
-  // });
-
-  var date = new Date(parseInt(time));
-  var options = { hour: "numeric", minute: "numeric" };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
-}
+import moment from 'moment';
 
 const EventsItem = ({ event }) => {
-  const date = formatDate(event.time);
-  const time = formatTime(event.time);
-  console.log(time);
+  const date = moment(event.time).format("dddd, MMMM Do");
+  const time = moment(event.time).format("YYYY, h:mm a");
 
   return (
     <ListGroupItem className="group-event-item" >
       <Container>
         <Row xs="2">
           <Col xs="9" className="group-event-title">{event.title}</Col>
-          <Col xs="3" className="group-event-time">{date}</Col>
+          <Col xs="3" className="group-event-time">
+            <Col>{date}</Col>
+            <Col>{time}</Col>
+          </Col>
           <Col xs="9" className="group-event-description">{event.description}</Col>
           <Col xs="3" className="group-event-button"><Button outline color="secondary">Attend</Button>{' '}</Col>
         </Row>
