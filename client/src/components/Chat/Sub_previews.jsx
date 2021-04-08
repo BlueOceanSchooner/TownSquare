6320d43d96bc48f5c9344225907c7865dbdd3103
Colleen Handel <handelc22@gmail.com>
2021-04-08 15:32:40 -0500

    Fixed error in previews

--- client/src/components/Chat/Sub_previews.jsx
+++ client/src/components/Chat/Sub_previews.jsx
@@ -14,14 +14,25 @@ const Sub_previews = ({ userID, chats, active, changeActiveConversation, openNew
             <div className="message" key={otherUserID} name={otherUserID} onClick={changeActiveConversation}>
               <span className={Number(otherUserID) === Number(active) ? "active-bar" : null}></span>
               <i className="user-picture fas fa-user"></i>
-              <span className="preview-timestamp">{getProperTimestamp(conversation[conversation.length - 1].timestamp)}</span>
+              <span className="preview-timestamp">
+              {
+                conversation ?
+                getProperTimestamp(conversation[conversation.length - 1].timestamp)
+                : null
+              }
+              </span>
               <div className="content">
                 <span className="other-user-name">
                   {conversation[0].sender.user_id === userID ? `${conversation[0].receiver.first_name} ${conversation[0].receiver.last_name}` : `${conversation[0].sender.first_name} ${conversation[0].sender.last_name}`}
                 </span>
                 <br/>
                 <span className="last-message">
-                  {conversation[conversation.length - 1].sender.user_id === userID ? "You: " : `${conversation[conversation.length - 1].sender.first_name} ${conversation[conversation.length - 1].sender.last_name}: `}
+                  {
+                    conversation ?
+                    conversation[conversation.length - 1].sender.user_id === userID ? "You: " : `${conversation[conversation.length - 1].sender.first_name} ${conversation[conversation.length - 1].sender.last_name}: `
+                    : null
+                  }
+
                   {conversation[conversation.length - 1].message}
                 </span>
               </div>
