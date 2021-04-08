7bd7c4e3626a1dbd75057366734b34c523ce6383
Ross Loney <majoross@gmail.com>
2021-04-08 15:02:09 -0500

    Replace pic of Obama laughing with category-related photos for groups

--- client/src/components/homepage/GroupItem.jsx
+++ client/src/components/homepage/GroupItem.jsx
@@ -6,7 +6,7 @@ const GroupItem = ({ group }) => {
     <div className="col-3" style={{cursor: 'pointer'}}>
       <Link to={`/groups/${group.group_id}`}>
         <div className="card border border-dark mt-3 position-relative">
-          <img className="card-img-top" src="https://cms.qz.com/wp-content/uploads/2016/07/rtx2c9ws.jpg?quality=75&strip=all&w=1200&h=900&crop=1" alt="person laughing" />
+          <img className="card-img-top" src={group.image_url} alt="person laughing" />
           <div className="card-body pt-0">
             <h5 className="card-title text-center pt-1" style={{height: '2em', fontSize: '1.2em'}}>{group.group_name}</h5>
           </div>
@@ -14,8 +14,7 @@ const GroupItem = ({ group }) => {
         </div>
       </Link>
     </div>
-
   );
-};
+}
 
 export default GroupItem;
