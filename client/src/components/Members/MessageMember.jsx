489926138e91342ee212da702a7204570e948ee2
Colleen Handel <handelc22@gmail.com>
2021-04-09 10:36:48 -0500

    Refactored message member component to not be clickable if no user signed in

--- client/src/components/Members/MessageMember.jsx
+++ client/src/components/Members/MessageMember.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const Member = ({ name, id, onClick, userID, style }) => {
+  console.log(userID);
   if (Number(id) === Number(userID)) {
     return (
       <div style={style ?? null}>
         You
       </div>
     );
+  }
+  if (!userID) {
+    return (
+      <div style={style ?? null}>
+        {name}
+      </div>
+    )
   }
   return (
     <div style={style ?? null} name={id} onClick={onClick} className={'message-member'}>
